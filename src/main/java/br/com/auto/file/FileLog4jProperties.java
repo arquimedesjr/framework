fbe2ea2965d7448cfd9aaa4dc79f9876d05de0d1
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-02-12 13:38:31 -0300

    framework

---
+++ src/main/java/br/com/auto/file/FileLog4jProperties.java
@@ -0,0 +1,45 @@
+package br.com.auto.file;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class FileLog4jProperties extends FileUtil {
+	
+	private Logger logger = Logger.getLogger(FileLog4jProperties.class);
+
+	private static FileLog4jProperties configProperties;
+
+	public final static String path = "src" + File.separator + "test" + File.separator + "resources" + File.separator
+			+ "config";
+	public final static String archive = "log4j.properties";
+
+	public static String dirProperties = path + File.separator +archive;
+
+	public static FileLog4jProperties getInstance() {
+		if (configProperties == null) {
+			configProperties = new FileLog4jProperties();
+		}
+		return configProperties;
+	}
+
+	public void createProperties() {
+		logger.info("Criação do arquivo log4j.propriedades");
+		boolean cond = false;
+
+		cond = createrFile(path, archive);
+
+		if (cond) {
+			List<String> listProperties = new ArrayList<String>();
+			listProperties.add("browser_name=CHROME");
+			listProperties.add("url=https://www.google.com/");
+			listProperties.add("dir_report_html=target/report-automation/report.html");
+			whiter(dirProperties, listProperties);
+			
+		}
+
+	}
+
+}
